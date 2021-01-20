61445ff3b90951b9b3dad91db8d9211f946d1d95
Jenna Weller <65751108+JennaWeller@users.noreply.github.com>
2021-01-19 19:18:47 -0700

    FirstCommit

--- wwwroot/js/JavaScript.js
+++ wwwroot/js/JavaScript.js
@@ -1,11 +1,65 @@
-﻿var submit = document.getElementById("submitButton");
+﻿//declaring variables
+var studentName;
+var assignments;
+var groupProject;
+var quizzes;
+var exams;
+var intex;
+var finalGrade;
+var letterGrade;
+var submit = document.getElementById("calculategrade");
+
 submit.addEventListener("click", function () {
-    alert("From =" + document.getElementById("from").value + ", " + 
-        "Email = " + document.getElementById("email").value + ", " + "Subject = " + document.getElementById("subject").value + ", " +
-        "Message = " + document.getElementById("message").value);
+    //grabbing the value from the input boxes and assigning it to variables to use in grade calculation 
+    studentName = document.getElementById("studentName").value;
+    assignments = parseInt(document.getElementById("assignments").value);
+    groupProject = parseInt(document.getElementById("groupProject").value);
+    quizzes = parseInt(document.getElementById("quizzes").value);
+    exams = parseInt(document.getElementById("exams").value);
+    intex = parseInt(document.getElementById("intex").value);
+
+
+    finalGrade = (assignments * .50) + (groupProject * .10) + (quizzes * .10) + (exams * .20) + (intex * .10);
+    //if statements to figure out the letter grade based on their final grade percentage 
+    if (finalGrade >= 94) {
+        letterGrade = "A";
+    }
+    else if (finalGrade >= 90) {
+        letterGrade = "A-";
+    }
+    else if (finalGrade >= 87) {
+        letterGrade = "B+";
+    }
+    else if (finalGrade >= 84) {
+        letterGrade = "B";
+    }
+    else if (finalGrade >= 80) {
+        letterGrade = "B-";
+    }
+    else if (finalGrade >= 77) {
+        letterGrade = "C+";
+    }
+    else if (finalGrade >= 74) {
+        letterGrade = "C";
+    }
+    else if (finalGrade >= 70) {
+        letterGrade = "C-";
+    }
+    else if (finalGrade >= 67) {
+        letterGrade = "D+";
+    }
+    else if (finalGrade >= 64) {
+        letterGrade = "D";
+    }
+    else if (finalGrade >= 60) {
+        letterGrade = "D-";
+    }
+    else {
+        letterGrade = "E";
+    }
+    //pop-up to say what grade the student earned - both letter and percentage 
+    alert(studentName + ' earned a final grade of a ' + letterGrade + " with a percentage of " + finalGrade + "%");
+
 });
-
-
-
 
 
